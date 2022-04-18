b0ace48ffca51a580967bc70ba722ed578a5ad53
Jacky Zeng <Jin.JJ.Zeng@ibm.com>
2022-04-18 17:41:52 +0800

    add prototype pattern

---
+++ src/main/java/com/jackyzeng/designpatterns/creates/prototype/ConcretePrototype.java
@@ -0,0 +1,26 @@
+package com.jackyzeng.designpatterns.creates.prototype;
+
+public class ConcretePrototype extends Prototype {
+
+    private final String field;
+
+    public ConcretePrototype(String field) {
+        this.field = field;
+    }
+
+    @Override
+    public Prototype customClone() {
+        return new ConcretePrototype(field);
+    }
+
+    @Override
+    public String toString() {
+        return field;
+    }
+
+    public static void main(String[] args) {
+        ConcretePrototype prototype = new ConcretePrototype("hello");
+        Prototype clonedPrototype = prototype.customClone();
+        System.out.println(clonedPrototype);
+    }
+}
---
+++ src/main/java/com/jackyzeng/designpatterns/creates/prototype/Prototype.java
@@ -0,0 +1,5 @@
+package com.jackyzeng.designpatterns.creates.prototype;
+
+public abstract class Prototype {
+    public abstract Prototype customClone();
+}
